0a5d99f72c52a28f4dbcb5804f46f2427983e477
Henrique <lhenrique.dev@gmail.com>
2021-04-06 22:35:52 -0300

    searchbox component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import React, { Component } from "react";
 import "./App.css";
 
 import CardList from "./components/CardList/CardList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 class App extends Component {
   state = {
     monsters: [],
+    searchField: "",
   };
 
   async componentDidMount() {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
+    const data = await res.json();
 
-    this.setState({ monsters: users });
+    this.setState({ monsters: data });
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder="search humans or monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
